50beb5b2a4e7ec2468196bb50f44a6fe0470f5e4
luoyin <luoyin1994@icloud.com>
2017-11-30 10:56:11 +0800

    begin ajax | async await

--- src/api/test.js
+++ src/api/test.js
@@ -4,5 +4,7 @@ const apiUrl = 'http://180.168.216.242:8071/api/1.0/pc/get-configuration-system-
 const tk     = 'tk=47b5c4921b01c920f1735a58a60a4caf';
 
 export async function getTestApiData() {
-    return await axios.get(apiUrl + '?' );
+    return await axios.get(apiUrl + '?').then(response => {
+
+    });
 }
