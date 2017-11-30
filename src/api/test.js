df67d6d093fc826fca93939597fa49685e6109b5
luoyin <luoyin1994@icloud.com>
2017-11-30 10:51:09 +0800

    add ajax.js

--- src/api/test.js
+++ src/api/test.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
-const apiUrl = 'http://localhost:3000/';
+const apiUrl = 'http://180.168.216.242:8071/api/1.0/pc/get-configuration-system-file';
+const tk     = 'tk=47b5c4921b01c920f1735a58a60a4caf';
 
-export function getTestApiData() {
-    return axios.get(apiUrl);
+export async function getTestApiData() {
+    return await axios.get(apiUrl + '?' );
 }
--- src/store/modules/testApi/testApi.store.js
+++ src/store/modules/testApi/testApi.store.js
@@ -4,23 +4,24 @@ export default {
     namespaced: true,
     state     : {
         title: '',
-        msg  : '',
+        msg  : ''
     },
     getter    : {},
     mutations : {
         changeState(state, obj) {
             state[obj.name] = obj.value;
-        },
+        }
     },
     actions   : {
         async getTestApiData({commit}) {
             let testApiData = await api.getTestApiData();
-            Object.keys(testApiData.data).forEach(key => {
-                commit('changeState', {
-                    name : key,
-                    value: testApiData.data[key],
-                });
-            });
-        },
-    },
+            console.log(testApiData.data);
+            // Object.keys(testApiData.data).forEach(key => {
+            //     commit('changeState', {
+            //         name : key,
+            //         value: testApiData.data[key],
+            //     });
+            // });
+        }
+    }
 };
